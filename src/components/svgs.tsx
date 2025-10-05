abdd18cdca53f024312476f5b26de38549c6f85f
ayvi-0001 <alankvickers+github@gmail.com>
2025-10-05 11:17:34 -0700

    feat: add function/weight svgs

--- src/components/svgs.tsx
+++ src/components/svgs.tsx
@@ -1,4 +1,6 @@
 import EditSquare from "./svgs/edit-square";
+import Function from "./svgs/function";
 import ManageHistory from "./svgs/manage-history";
+import Weight from "./svgs/weight";
 
-export { ManageHistory, EditSquare };
+export { ManageHistory, EditSquare, Function, Weight };
