08b7f3f34a4f9a49a3a3f901b904cba984f44d3c
ayvi-0001 <alankvickers+github@gmail.com>
2025-10-07 17:15:47 -0700

    refactor: decouple ComboboxForm

---
+++ src/components/combobox-form.tsx
@@ -0,0 +1,72 @@
+import * as React from "react";
+
+import { Command as CommandPrimitive } from "cmdk";
+import { ChevronsUpDown } from "lucide-react";
+
+import { cn } from "@/lib/utils";
+
+import { Button } from "@/components/ui/button";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
+import { FormControl } from "@/components/ui/form";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+
+export default function ComboboxForm({
+  selectedValue,
+  inputPlaceholder,
+  emptyPlaceholder,
+  commandItems,
+  state,
+  setState,
+  buttonProps,
+  commandProps,
+  commandListProps,
+}: {
+  selectedValue?: any;
+  inputPlaceholder: string;
+  emptyPlaceholder: string;
+  commandItems: React.ReactElement<typeof CommandItem>[];
+  onSelect?: (value: string) => void;
+  state?: boolean;
+  setState?: React.Dispatch<React.SetStateAction<boolean>>;
+  buttonProps?: React.ComponentPropsWithoutRef<"button">;
+  commandProps?: React.ComponentProps<typeof CommandPrimitive>;
+  commandListProps?: React.ComponentProps<typeof CommandPrimitive.List>;
+}) {
+  return (
+    <Popover open={state} onOpenChange={setState}>
+      <PopoverTrigger asChild>
+        <FormControl>
+          <Button
+            {...buttonProps}
+            variant="outline"
+            role="combobox"
+            className={cn(
+              "justify-between",
+              !selectedValue && "text-muted-foreground",
+              buttonProps?.className,
+            )}
+          >
+            {selectedValue ?? inputPlaceholder}
+            <ChevronsUpDown className="opacity-50" />
+          </Button>
+        </FormControl>
+      </PopoverTrigger>
+      <PopoverContent className="p-0">
+        <Command {...commandProps}>
+          <CommandInput placeholder={inputPlaceholder} className="h-fit" />
+          <CommandList {...commandListProps}>
+            <CommandEmpty>{emptyPlaceholder}</CommandEmpty>
+            <CommandGroup>{...commandItems}</CommandGroup>
+          </CommandList>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  );
+}
---
+++ src/components/routines/edit-daily-form.tsx
@@ -0,0 +1,220 @@
+import * as React from "react";
+import { ControllerRenderProps, useForm } from "react-hook-form";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { invoke } from "@tauri-apps/api/core";
+import clsx from "clsx";
+import { Check } from "lucide-react";
+import { z } from "zod";
+
+import { camelCaseToTitleCase } from "@/lib/string";
+import { cn } from "@/lib/utils";
+
+import ComboboxForm from "@/components/combobox-form";
+import { CommandItem } from "@/components/ui/command";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import Input from "@/components/ui/input";
+
+import type { RoutineAttrs } from "@/types/routines";
+import { Routine, RoutineType } from "@/types/routines";
+
+// NOTE: in ts, z.readonly() only affects objects, arrays, tuples, Set, and Map
+const formSchema = z.object({
+  // TODO(ayvi): allow reordering dailies http://ayvi:3000/ayvi/dailies/issues/39
+  ordinalPos: z.coerce.number<number>().readonly(),
+  valueId: z.string().readonly(),
+  routineId: z.string().readonly(),
+  name: z.string().nonempty(),
+  group: z.string().nonempty(),
+  type: z.enum(RoutineType.values()),
+  maxValue: z.coerce.number<number>().gt(0),
+  notes: z.string().nullable(),
+  streak: z.coerce.number<number>().nullable(),
+  nDays: z.coerce.number<number>().int().nullable(),
+  weekdays: z.string().nullable(),
+  // prettier-ignore
+  date:  z.coerce.date<Date>().nullable().transform((arg: Date | null) => (arg ? arg.toISOString().substring(0, 10) : null)),
+  // prettier-ignore
+  dateStarted:  z.coerce.date().nullable().transform((arg: Date | null) => (arg ? arg.toISOString().substring(0, 10) : null)),
+  dateArchived: z.iso.date().nullable(),
+  // date: z.codec(z.iso.datetime().nullable(), z.date().nullable(), { decode: (isoString: string | null): Date | null => isoString ? new Date(isoString) : null, encode: (date: Date | null): string | null => date ? date.toISOString().substring(0, 10) : null, }),
+  // dateStarted: z.codec(z.iso.datetime().nullable(), z.date().nullable(), { decode: (isoString: string | null): Date | null => isoString ? new Date(isoString) : null, encode: (date: Date | null): string | null => date ? date.toISOString().substring(0, 10) : null, }),
+  // dateArchived: z.codec(z.iso.datetime().nullable(), z.date().nullable(), { decode: (isoString: string | null): Date | null => isoString ? new Date(isoString) : null, encode: (date: Date | null): string | null => date ? date.toISOString().substring(0, 10) : null, }),
+  value: z.coerce.number<number>().gt(0).nullable().readonly(),
+  weight: z.coerce.number<number>().gt(0),
+  weightedValue: z.coerce.number<number>().gt(0).nullable().readonly(),
+  timeMin: z.iso.time().nullable(),
+  timeMax: z.iso.time().nullable(),
+  timeBucketMin: z.coerce.number<number>().readonly(),
+  timeBucketMax: z.coerce.number<number>().readonly(),
+});
+
+const formExcludeFields = [
+  "valueId",
+  "routineId",
+  "date",
+  "dateStarted",
+  "streak",
+  "value",
+  "weightedValue",
+  "timeBucketMin",
+  "timeBucketMax",
+] as const;
+
+type ExcludedField = (typeof formExcludeFields)[number];
+
+const formReadOnlyFields = ["ordinalPos"] as const;
+
+type ReadOnlyField = (typeof formReadOnlyFields)[number];
+
+export default function EditDailyForm({
+  routine,
+  dailyFormRef,
+  onRefreshAction,
+}: {
+  routine: Routine;
+  dailyFormRef: React.RefObject<HTMLFormElement | null>;
+  onRefreshAction: () => void;
+}): React.ReactElement {
+  let originalValues = routine as unknown as z.infer<typeof formSchema>;
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    /* @ts-expect-error: 2719 */
+    resolver: zodResolver(formSchema),
+    mode: "onChange",
+    defaultValues: originalValues,
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>): Promise<void> => {
+    // TODO(ayvi): send new values only?
+    // import { ValueOf } from "next/dist/shared/lib/constants";
+    // let changes = {};
+    // Object.keys(form.formState.dirtyFields).forEach((key: string) => {
+    //   changes[key as keyof typeof Object.prototype] = values[
+    //     key as keyof Routine
+    //   ] as ValueOf<never>;
+    // });
+    // console.debug(`changes: ${JSON.stringify(changes)}`);
+
+    await invoke<null>("update_daily", {
+      original_daily: originalValues,
+      new_daily: values,
+    });
+
+    onRefreshAction && onRefreshAction();
+  };
+
+  const [popoverOpen, setPopoverOpen] = React.useState<boolean>(false);
+
+  type Field = ControllerRenderProps<z.infer<typeof formSchema>>;
+
+  return (
+    <Form {...form}>
+      <form
+        ref={dailyFormRef}
+        /* @ts-expect-error: 2345 */
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-8"
+      >
+        {(Object.keys(routine) as RoutineAttrs[])
+          .filter((value: string) => !formExcludeFields.includes(value as ExcludedField))
+          .map((attr: RoutineAttrs, idx: number) => {
+            form.formState.errors[attr] &&
+              console.debug(
+                `Error submitting form for field ${attr}: ${form.formState.errors[attr].message}`,
+              );
+
+            let render;
+            if (attr == "type") {
+              render = ({ field }: { field: Field }) => (
+                <FormItem className="w-full bg-black">
+                  <FormLabel>
+                    {camelCaseToTitleCase(attr)}
+                    <p className="text-gray-500 italic">
+                      ({Routine.RustTypes[attr as keyof typeof Routine.RustTypes]})
+                    </p>
+                  </FormLabel>
+                  <ComboboxForm
+                    selectedValue={
+                      field.value
+                        ? `${RoutineType.find(field.value as string)}`
+                        : "select routine type"
+                    }
+                    inputPlaceholder={field.name}
+                    emptyPlaceholder="no matches..."
+                    state={popoverOpen}
+                    setState={setPopoverOpen}
+                    commandProps={{ className: "bg-black text-white" }}
+                    commandListProps={{ className: "bg-black text-white" }}
+                    buttonProps={{ className: " bg-black" }}
+                    commandItems={RoutineType.values().map(type => (
+                      <CommandItem
+                        className="bg-black text-white"
+                        value={type}
+                        key={type}
+                        onSelect={() => {
+                          form.setValue(field.name, type);
+                          setPopoverOpen(false);
+                        }}
+                      >
+                        {type}
+                        <Check
+                          className={cn(clsx(type === field.value ? "opacity-100" : "opacity-0"))}
+                        />
+                      </CommandItem>
+                    ))}
+                  />
+                </FormItem>
+              );
+            } else {
+              render = ({ field }: { field: Field }) => (
+                <FormItem>
+                  <FormLabel>
+                    {camelCaseToTitleCase(attr)}
+                    <p className="text-gray-500 italic">
+                      ({Routine.RustTypes[attr as keyof typeof Routine.RustTypes]})
+                    </p>
+                  </FormLabel>
+                  <FormControl>
+                    {/* @ts-expect-error: 2322 */}
+                    <Input
+                      id={`${idx}-${routine.valueId}`}
+                      key={`${idx}-${routine.valueId}`}
+                      placeholder="none"
+                      readOnly={formReadOnlyFields.includes(field.name as ReadOnlyField)}
+                      autoComplete="false"
+                      aria-autocomplete="none"
+                      {...form.register(attr)}
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormDescription className="text-gray-500 italic">
+                    {formReadOnlyFields.includes(field.name as ReadOnlyField) && `(readonly)`}
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              );
+            }
+
+            return (
+              <FormField
+                key={idx}
+                /* @ts-expect-error: 2322 */
+                control={form.control}
+                name={attr}
+                render={render}
+              />
+            );
+          })}
+      </form>
+    </Form>
+  );
+}
