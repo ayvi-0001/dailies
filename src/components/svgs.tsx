8774c183149bafe0473c6af6495e734813abdac4
ayvi-0001 <alankvickers+github@gmail.com>
2025-10-10 07:14:17 -0700

    refactor: svgs module

--- src/components/svgs.tsx
+++
@@ -1,8 +0,0 @@
-import EditSquare from "./svgs/edit-square";
-import Function from "./svgs/function";
-import ManageHistory from "./svgs/manage-history";
-import NoteStack from "./svgs/note-stack";
-import TrailLength from "./svgs/trail-length";
-import Weight from "./svgs/weight";
-
-export { ManageHistory, EditSquare, Function, Weight, NoteStack, TrailLength };
---
+++ src/components/svgs/index.tsx
@@ -0,0 +1,8 @@
+import EditSquare from "./edit-square";
+import Function from "./function";
+import ManageHistory from "./manage-history";
+import NoteStack from "./note-stack";
+import TrailLength from "./trail-length";
+import Weight from "./weight";
+
+export { ManageHistory, EditSquare, Function, Weight, NoteStack, TrailLength };
