acc9413c35d058e2ad7ffa0c0cea77c101366894
ayvi-0001 <alankvickers+github@gmail.com>
2025-10-13 10:26:45 -0700

    feat: remove window decorations

--- src/app/background.tsx
+++ src/app/background.tsx
@@ -1,8 +1,13 @@
-import React from "react";
+"use client";
 
+import * as React from "react";
+
+import { Window, getCurrentWindow } from "@tauri-apps/api/window";
 import Image from "next/image";
 
 import { cn } from "@/lib/utils";
+
+/* eslint-disable @typescript-eslint/no-unused-expressions */
 
 type BackgroundProps = {
   src: string;
@@ -19,8 +24,21 @@ type BackgroundProps = {
 export default function BackgroundImage(background_props: BackgroundProps): React.ReactElement {
   const { src, alt, style, className, props, imageProps, children } = background_props;
 
+  React.useEffect(() => {
+    document.getElementById("background")?.addEventListener("mousedown", (event: MouseEvent) => {
+      const appWindow: Window = getCurrentWindow();
+      if (event.buttons === 1) {
+        event.detail === 2 ? appWindow.toggleMaximize() : appWindow.startDragging();
+      }
+    });
+  }, []);
+
   return (
-    <div className={cn("z--2 fixed top-0 left-0 h-screen w-screen", className)} {...props}>
+    <div
+      id="background"
+      className={cn("z--2 fixed top-0 left-0 h-screen w-screen", className)}
+      {...props}
+    >
       <Image
         alt={alt ?? ""}
         fill
