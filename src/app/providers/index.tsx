7cdcdeb6aabb280eb87512cb398c66e06cfea410
ayvi-0001 <alankvickers+github@gmail.com>
2025-12-27 06:10:44 -0800

    refactor: add router to providers component

--- src/app/providers/index.tsx
+++ src/app/providers/index.tsx
@@ -1,14 +1,25 @@
 "use client";
 
 import { HeroUIProvider } from "@heroui/react";
+import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
+import { useRouter } from "next/navigation";
+
 import AppMetaProvider from "./app-meta";
 import UserProvider from "./user";
+
+declare module "@react-types/shared" {
+  interface RouterConfig {
+    routerOptions: NonNullable<Parameters<ReturnType<typeof useRouter>["push"]>[1]>;
+  }
+}
 
 export default function Providers({
   children,
 }: Readonly<{ children?: React.ReactNode }>): React.ReactNode {
+  const router: AppRouterInstance = useRouter();
+
   return (
-    <HeroUIProvider>
+    <HeroUIProvider navigate={router.push}>
       <AppMetaProvider>
         <UserProvider>{children}</UserProvider>
       </AppMetaProvider>
