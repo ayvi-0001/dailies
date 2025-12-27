f8eaf6c7472871bf8f76da7b721f7acb549674de
ayvi-0001 <alankvickers+github@gmail.com>
2025-12-27 06:13:19 -0800

    feat: separate info/error listeners from backend

--- src/app/listener.tsx
+++ src/app/listener.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react";
 
-import { Event, listen } from "@tauri-apps/api/event";
+import { Event, UnlistenFn, listen } from "@tauri-apps/api/event";
 import dynamic from "next/dynamic";
 import { toast } from "sonner";
 
@@ -10,10 +10,17 @@ import type { ErrorMessage } from "@/types/errors";
 
 export default function Toaster(): React.ReactElement {
   React.useEffect(() => {
-    const err: string = "tauri://error";
-    listen<ErrorMessage>(err, (event: Event<ErrorMessage>) => {
-      toast.error(err, { description: event.payload.message });
+    const unlistenInfo = listen<ErrorMessage>("tauri://info", (event: Event<ErrorMessage>) => {
+      toast.info("tauri://info", { description: event.payload.message });
     });
+    const unlistenError = listen<ErrorMessage>("tauri://error", (event: Event<ErrorMessage>) => {
+      toast.error("tauri://error", { description: event.payload.message });
+    });
+
+    return () => {
+      unlistenInfo.then((off: UnlistenFn) => off());
+      unlistenError.then((off: UnlistenFn) => off());
+    };
   }, []);
 
   const Toaster = dynamic(() => import("sonner").then(mod => mod.Toaster), {
@@ -23,13 +30,15 @@ export default function Toaster(): React.ReactElement {
   return (
     <Toaster
       closeButton
+      richColors
+      className="z-999"
       duration={10000}
       expand={true}
       gap={4}
       position="bottom-right"
-      richColors
       theme="dark"
       toastOptions={{ classNames: { title: "text-md font-bold" } }}
+      visibleToasts={5}
     />
   );
 }
