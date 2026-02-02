6953e53f08ebc6265446ea2f816962262d874043
ayvi-0001 <alankvickers+github@gmail.com>
2026-02-02 10:00:20 -0800

    feat: add previous streak to points

--- src/components/daily/points.tsx
+++ src/components/daily/points.tsx
@@ -1,14 +1,16 @@
 import * as React from "react";
 
 import * as heroui from "@heroui/react";
+import clsx, { ClassValue } from "clsx";
 import { HTMLMotionProps } from "framer-motion";
 import { FlameIcon } from "lucide-react";
 
 import * as svgs from "@/components/svgs";
 import { roundTo } from "@/lib/number";
+import { cn } from "@/lib/utils";
 import type { Option } from "@/types/option";
 
-import type { Daily } from "./types";
+import { type Daily, Quest } from "./types";
 
 type PointsDisplayProps = {
   daily: Daily;
@@ -19,61 +21,97 @@ type PointsDisplayProps = {
 export default function PointsDisplay(props: PointsDisplayProps): React.ReactNode {
   const { daily, points: _, totalWeight } = props;
 
-  const pointsWeighted: Option<string> = daily.pointsWeighted
-    ? roundTo(daily.pointsWeighted, 2).toString()
+  const pointsWeighted: Option<number> = daily.pointsWeighted
+    ? roundTo(daily.pointsWeighted, 2)
     : null;
   const dailyPointsContribution: number = daily.pointsWeighted
     ? roundTo((daily.pointsWeighted / totalWeight) * 100, 2)
     : 0;
+  const dailyPointsMaxContribution: number = roundTo(
+    (daily.weight / (daily.points === null ? totalWeight + daily.weight : totalWeight)) * 100,
+    2,
+  );
+
+  const textClassValue: ClassValue = cn(
+    "text-[0.60rem] leading-none font-bold",
+    clsx(
+      (daily.type == Quest.Type.QO && daily.points === null) || daily.points === null
+        ? "opacity-50"
+        : "opacity-100",
+    ),
+  );
 
   const DefaultPoints = (): React.ReactElement => (
     <Tooltip content="Starting Points">
       <div className="flex flex-row items-center justify-self-end">
-        <p className="text-[0.60rem] leading-none font-bold text-black empty:w-3">
-          {`{${daily.defaultPoints}}`}
-        </p>
+        <p className={textClassValue}>{`{${daily.defaultPoints}}`}</p>
       </div>
     </Tooltip>
   );
 
   const Weight = (): React.ReactElement => (
     <Tooltip content="Weight">
-      <div className="flex flex-row items-center justify-self-end">
-        <p className="text-[0.60rem] leading-none font-bold text-black empty:w-3">{daily.weight}</p>
-        <svgs.Weight className="mb-[2] size-4 fill-black" />
+      <div className="flex flex-row items-center">
+        <span className={textClassValue}>{`(${dailyPointsMaxContribution.toFixed(2)}%)`}</span>
+        <span className="mx-[2] text-xs font-bold text-gray-500">/</span>
+        <span className={textClassValue}>{daily.weight.toFixed(2)}</span>
+        <svgs.Weight className={cn(textClassValue, "mb-[2] size-4 fill-black")} />
       </div>
     </Tooltip>
   );
 
   const WeightedPoints = (): React.ReactElement => {
-    const displayPoints: React.ReactNode =
-      daily.points === null
-        ? ""
-        : pointsWeighted && Number.isFinite(dailyPointsContribution)
-          ? `(${dailyPointsContribution}%) ${pointsWeighted}`
-          : 0;
-
     return (
       <Tooltip content="Weighted Points">
         <div className="flex flex-row items-center">
-          <p className="flex-1 justify-self-start align-middle text-[0.60rem] leading-none font-bold text-black empty:w-3">
-            {displayPoints}
-          </p>
-          <svgs.Function className="size-4 fill-black" />
+          {daily.points !== null && (
+            <>
+              <span className={textClassValue}>
+                {daily.points === null
+                  ? ""
+                  : pointsWeighted
+                    ? `(${dailyPointsContribution.toFixed(2)}%)`
+                    : `(${(0).toFixed(2)}%)`}
+              </span>
+              <span className="mx-[2] text-xs font-bold text-gray-500">/</span>
+              <span className={textClassValue}>
+                {pointsWeighted && Number.isFinite(dailyPointsContribution)
+                  ? pointsWeighted.toFixed(2)
+                  : `${(0).toFixed(2)}`}
+              </span>
+            </>
+          )}
+          <svgs.Function className={cn(textClassValue, "size-4 fill-black")} />
         </div>
       </Tooltip>
     );
   };
 
   const Streak = (): React.ReactElement => {
+    const continueStreak: boolean =
+      daily.streak === 0 && !!(daily.previousStreak && daily.previousStreak > 0);
+
     return (
       <Tooltip content="Streak">
         <div className="flex flex-row items-center">
-          <p className="text-[0.60rem] leading-none font-bold text-black italic empty:w-3">
-            {`${daily.streak}`}
-            {daily.streakTarget && `/${daily.streakTarget}`}
+          <p className={cn(textClassValue, "italic", clsx(continueStreak && "opacity-50"))}>
+            {daily.streak || daily.previousStreak ? (
+              <span>
+                {daily.streak && daily.streak > 0
+                  ? daily.streak
+                  : daily.previousStreak
+                    ? daily.previousStreak + 1
+                    : ""}
+              </span>
+            ) : (
+              <span className="text-black/50">{daily.previousStreak}</span>
+            )}
+            <span>{daily.streakTarget && `/${daily.streakTarget}`}</span>
           </p>
-          <FlameIcon className="mb-[2] ml-[2] size-3" fill="#000000" />
+          <FlameIcon
+            className={cn("mb-[2] ml-[2] size-3", clsx(continueStreak && "opacity-50"))}
+            fill="#000000"
+          />
         </div>
       </Tooltip>
     );
@@ -95,7 +133,12 @@ export default function PointsDisplay(props: PointsDisplayProps): React.ReactNod
   }
 
   const elements: React.ReactElement[] = [];
-  if (daily.streak) elements.push(<Streak />);
+  if (
+    (daily.streak && daily.streak > 0) ||
+    daily.streakTarget ||
+    (daily.previousStreak && daily.previousStreak > 0)
+  )
+    elements.push(<Streak />);
   elements.push(<DefaultPoints />, <Weight />);
 
   return (
