b2e3ed0d94b9aa9959cc1a27695a5bf901e0944a
ayvi-0001 <alankvickers+github@gmail.com>
2026-04-28 09:46:28 -0700

    refactor: useWindowSize in app meta provider

--- src/app/providers/app-meta.tsx
+++ src/app/providers/app-meta.tsx
@@ -2,19 +2,18 @@
 
 import * as React from "react";
 
-import { UnlistenFn, listen } from "@tauri-apps/api/event";
-import { getCurrentWindow } from "@tauri-apps/api/window";
+import * as ReactUse from "@reactuses/core";
 import { platform as getPlatform } from "@tauri-apps/plugin-os";
 import type { Platform } from "@tauri-apps/plugin-os";
 import ok from "assert";
 
-import type { TauriWindowResizeEvent } from "@/types/events";
 import { Option } from "@/types/option";
 
 export type AppMetaState = {
   platform: Option<string>;
-  orientation: Option<string>;
-  windowWidth: Option<number>;
+  orientation?: Option<string>;
+  width: Option<number>;
+  height: Option<number>;
 };
 
 const AppMetaContext = React.createContext<AppMetaState | null>(null);
@@ -22,44 +21,19 @@ const AppMetaContext = React.createContext<AppMetaState | null>(null);
 export default function AppMetaProvider({
   children,
 }: Readonly<{ children?: React.ReactNode }>): React.ReactNode {
-  const [orientation, setOrientation] = React.useState<Option<string>>(null);
-  const [windowWidth, setWindowWidth] = React.useState<Option<number>>(null);
   const [platform, setPlatform] = React.useState<Option<Platform>>(null);
+  const { width, height } = ReactUse.useWindowSize();
+  const [orientationState] = ReactUse.useOrientation();
 
-  React.useEffect((): void => setPlatform(getPlatform()), []);
+  // Must happen in useEffect otherwise window might not be defined.
+  ReactUse.useOnceEffect(() => setPlatform(getPlatform()));
 
-  React.useEffect(() => {
-    const getInitialWidth = async () => {
-      const innerSize = await getCurrentWindow().innerSize();
-      setWindowWidth(innerSize.width);
-    };
-
-    getInitialWidth();
-
-    const unlisten: Promise<UnlistenFn> = listen(
-      "tauri://resize",
-      (event: TauriWindowResizeEvent): void => {
-        setWindowWidth(event.payload.width);
-      },
-    );
-
-    const listener = async (e: MediaQueryListEventMap["change"]): Promise<void> => {
-      const innerSize = await getCurrentWindow().innerSize();
-      setWindowWidth(innerSize.width);
-      setOrientation(e.media);
-    };
-
-    const mediaQuery: MediaQueryList = window.matchMedia("(orientation: portrait)");
-
-    mediaQuery.addEventListener("change", listener);
-
-    return () => {
-      unlisten.then((off: UnlistenFn) => off());
-      mediaQuery.removeEventListener("change", listener);
-    };
-  }, [platform]);
-
-  const value: AppMetaState = { platform, orientation, windowWidth };
+  const value: AppMetaState = {
+    platform,
+    orientation: orientationState.type,
+    width: width,
+    height: height,
+  };
 
   return <AppMetaContext.Provider value={value}>{children}</AppMetaContext.Provider>;
 }
