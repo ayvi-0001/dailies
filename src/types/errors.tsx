8336928d9090bf338dc0dbc4e5e6356622343118
ayvi-0001 <alankvickers+github@gmail.com>
2026-05-30 14:15:50 -0700

    refactor: add content message to AppError

--- src/types/errors.tsx
+++ src/types/errors.tsx
@@ -7,12 +7,14 @@ export type AppErrorContent = {
 export class AppError extends Error {
   public kind?: string;
   public title?: string;
+  public content?: string;
 
   constructor(err: AppErrorContent) {
     super(err.content);
     this.name = "AppError";
     this.kind = err.kind;
     this.title = err.title;
+    this.content = err.content;
 
     Object.setPrototypeOf(this, AppError.prototype);
   }
